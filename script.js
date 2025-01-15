fdc502d004b5d90ac9d2346707d8657084b20ecc
Mehmet Gökmen <mgokmen20@hotmail.com>
2025-01-15 14:54:43 +0100

    15 January end of the day

--- script.js
+++ script.js
@@ -5,22 +5,19 @@ const closeIcon = document.querySelector('.close');
 const fullscreenMenu = document.getElementById('fullscreenMenu');
 
 // Toggle the menu
-menuIcon.addEventListener('click', function() {
-    // If the menu is currently closed, show it
-    if (fullscreenMenu.style.display === 'none' || fullscreenMenu.style.display === '') {
-        fullscreenMenu.style.display = 'flex';
-        setTimeout(() => fullscreenMenu.classList.add('open'), 10); // Delay class addition for animation
-        hamburgerIcon.style.display = 'none'; // Hide hamburger icon
-        closeIcon.style.display = 'inline-block'; // Show close icon
+menuIcon.addEventListener('click', function () {
+    // Menü açık değilse aç
+    if (!fullscreenMenu.classList.contains('open')) {
+        fullscreenMenu.classList.add('open'); // CSS animasyon sınıfı eklenir
+        hamburgerIcon.style.display = 'none'; // Hamburger simgesini gizle
+        closeIcon.style.display = 'inline-block'; // Kapat simgesini göster
     } else {
-        fullscreenMenu.classList.remove('open');
-        setTimeout(() => {
-            fullscreenMenu.style.display = 'none';
-            hamburgerIcon.style.display = 'inline-block'; // Show hamburger icon
-            closeIcon.style.display = 'none'; // Hide close icon
-        }, 500); // Wait for animation to finish before hiding menu
+        fullscreenMenu.classList.remove('open'); // CSS animasyon sınıfı çıkarılır
+        hamburgerIcon.style.display = 'inline-block'; // Hamburger simgesini geri getir
+        closeIcon.style.display = 'none'; // Kapat simgesini gizle
     }
 });
+
 
 
 /* Here for Bootstrap slider stop with mauseover*/
