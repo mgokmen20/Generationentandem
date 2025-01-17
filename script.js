0386abe2d278d510a8397ec798f57885cd238698
Mehmet Gökmen <mgokmen20@hotmail.com>
2025-01-17 22:52:52 +0100

    17 Ocak 23:00

--- script.js
+++ script.js
@@ -21,27 +21,33 @@ menuIcon.addEventListener('click', function () {
     }
 });
 
+
+
 // Search Area
 const searchIcon = document.getElementById('searchIcon');
 const searchInput = document.getElementById('searchInput');
 
-searchIcon.addEventListener('click', function() {
+// Open or close the box when clicking on the search icon
+searchIcon.addEventListener('click', function () {
   searchInput.classList.toggle('open');
   if (searchInput.classList.contains('open')) {
-    searchInput.focus(); // Arama kutusu açıldığında focus'lanır
-    searchInput.style.width = '200px'; // Arama kutusu açıldığında genişliği ayarla
-    searchInput.style.right = '0';
+    searchInput.focus(); // Focuses when search box is opened
+    searchInput.style.width = '200px'; // Adjust width
   } else {
-    searchInput.style.width = '0'; // Arama kutusu kapandığında genişliği sıfırla
-    searchInput.style.right = '0';
+    searchInput.style.width = '0'; // Reset width on close
   }
 });
 
-// Input alanından çıkıldığında (blur eventi) arama kutusunu kapat
-searchInput.addEventListener('blur', function() {
+// Close the box when the mouse moves out of the box
+searchInput.addEventListener('mouseout', function () {
   searchInput.classList.remove('open');
-  searchInput.style.width = '0'; // Arama kutusu kapandığında genişliği sıfırla
-  searchInput.style.right = '0';
+  searchInput.style.width = '0'; // Reset width
+});
+
+// Close the search box when exiting the input field (blur event)
+searchInput.addEventListener('blur', function () {
+  searchInput.classList.remove('open');
+  searchInput.style.width = '0'; // Reset width
 });
 
 
